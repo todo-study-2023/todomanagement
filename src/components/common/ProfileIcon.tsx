bdf0ac5764a007e8a1790cde41c2a89d240937a5
sgsg9447 <sgsg9447@gmail.com>
2023-02-18 18:54:53 +0900

    feat : Fine Component Markup

--- src/components/common/ProfileIcon.tsx
+++ src/components/common/ProfileIcon.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 export default function ProfileIcon({ size }: { size?: Size }) {
-  return size === "sm" ? (
+  return size === "xs" ? (
+    <div className="profile-xs-icon"></div>
+  ) : size === "sm" ? (
     <div className="profile-sm-icon"></div>
   ) : (
     <div className="profile-lg-icon"></div>
