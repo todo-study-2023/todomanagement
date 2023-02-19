ccdd7aacc4b6bdc0a164843a9f97b07b74fbecc3
sgsg9447 <sgsg9447@gmail.com>
2023-02-19 16:52:22 +0900

    feat : Modal Component Markup

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.scss";
 import Button from "./components/common/Button";
 import Text from "./components/common/Text";
+import Code from "./views/code/Code";
 import Room from "./views/room/Room";
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
         Gkdl
       </select>
       <input type="text" /> */}
-      <Room />
+      {/* <Room /> */}
+      <Code/>
     </>
   );
 }
