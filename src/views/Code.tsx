1f6c237f8a2dd6e477be45d79d228a806ce01352
sgsg9447 <sgsg9447@gmail.com>
2023-02-20 18:19:24 +0900

    feat : router 처리

--- src/views/Code.tsx
+++ src/views/Code.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Code() {
-  return (
-    <div>Code</div>
-  )
+  return <div className="container">{/* Modal 컴포넌트 */}</div>;
 }
