eb96dbabc52853f35ac420e43d5e17cc63fa15e9
sgsg9447 <sgsg9447@gmail.com>
2023-03-01 17:59:42 +0900

    feat : fine component data binding any

--- src/views/room/Fine.tsx
+++ src/views/room/Fine.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Text from "../../components/common/Text";
 import * as Icon from "react-feather";
 import ProfileIcon from "../../components/common/ProfileIcon";
 import Title from "../../components/common/Title";
+import { useAPI } from "../../hooks/useAPI";
+import { fetchFine } from "../../services/api/fine";
 
 export default function Fine() {
+  const { data: fine } = useAPI<FineEntity>(fetchFine, { isFetch: true });
+  //9시 배열
+  //12시
+  //18시
+  //각각의 배열을 만들고 map돌리기
+
+  const refetchData = () => {
+    fine
+      .filter((f) => f.time === 9)
+      .map((f) => {
+        //TODO :리팩토링대상임
+        const fmember: any = (f.members as any).data;
+        return fmember.map((m: any) => {
+          console.log(m.data);
+        });
+        console.log(fmember);
+      });
+  };
+  useEffect(() => {
+    refetchData();
+  }, [fine]);
+
   return (
     <div className="item">
       <div className="item-title">
